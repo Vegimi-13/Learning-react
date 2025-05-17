6ccf5e589e119930b557128b2004ed51f899f089
Vegimi-13 <vegimahmetaj@gmail.com>
2025-05-18 00:03:15 +0200

    Storing Component Style File next to Components

--- front-end-course/src/App.jsx
+++ front-end-course/src/App.jsx
@@ -1,18 +1,10 @@
-import Header from "./components/Header.jsx";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 import { CORE_CONCEPTS } from "./data.js";
 
 // console.log(test);
 
-function CoreConcept({ title, image, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p> {description} </p>
-    </li>
-  );
-}
 
 function App() {
   return (
@@ -28,6 +20,7 @@ function App() {
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
+            {/* Spread Operator ...CORE_CONCEPTS[0] */}
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
